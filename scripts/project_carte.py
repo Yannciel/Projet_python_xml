cc6d1307a43d4479ffe5316b47921980fc84d21b
Mingqiang <yann.losay@gmail.com>
2018-01-02 20:59:00 +0100

    projeter fontaines et zones touristique dans une carte

--- scripts/project_carte.py
+++ scripts/project_carte.py
@@ -21,15 +21,29 @@
 #							- <autres élèments> :
 ###############
 
-from lxml import etree
+from lxml import etree as ET
 import folium
-FICHIER = '../XML/fontaines-a-boire.xml'
-tree = etree.parse(FICHIER)
+
+def mark_touristes(nomfichier):
+    """
+    Projection des monuments dans la carte
+    Arg: fichier XML de coordonnés des zones touristes
+    Retourne: une carte OpenStreetMap
+    """
+    carte_touristes = folium.Map(location=[48.87, 2.33],zoom_start=13)
+    tree = ET.parse(nomfichier)
+
+    for elem in tree.iter(tag='zone'):
+        name = elem.find('name').text
+        coor= elem.find('geo_point').text
+        folium.CircleMarker(location=[coor],radius=200,fill_color='#3186cc',popup=name).add_to(carte_touristes)
+    return carte_touristes
+
 
 def mark_fontaine(carte,nomfichier):
     """
     Projection de tournages dans une carte OpenStreetMap
-    Arg: la carte OpenStreetMap qui a déjà marqué les monuments, et le fichier XML des films
+    Arg: la carte OpenStreetMap qui a déjà marqué zones touristes, et le fichier XML des fontaines
     """
     tree = ET.parse(nomfichier)
     for elem in tree.iter(tag = 'fontaine'):
@@ -37,4 +51,9 @@ def mark_fontaine(carte,nomfichier):
         folium.CircleMarker(location=[coor],radius=1).add_to(carte)
     return carte
 
-    
+
+print("Projection des zones touristes et toutes les fontaines dans une carte...")
+carte_touristesfontaines= mark_fontaine(mark_touristes("../xml/zones-touristiques-internationales.xml"),"../xml/fontaines-a-boire.xml")
+print("Réussir à créer la carte!")
+carte_touristesfontaines.save('../cartes/carte_zonestouristiques_fontaines.html')
+print("Fin")
